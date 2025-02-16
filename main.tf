3841ad1825c721d870b3cfe924d7008bc800cdaa
ondyke <dikeonuohajohn@gmail.com>
2025-02-16 20:11:52 +0100

    Added Terraform EC2 configuration

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
 provider "aws" {
-  region = "us-east-1"  
+  region = "us-east-1"  # Change to your preferred AWS region
+}
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c518311db5640eff"
+  ami           = "ami-0c518311db5640eff" # Ubuntu AMI (change based on region)
   instance_type = "t2.micro"
 
   tags = {
     Name = "Terraform-EC2"
   }
 }
-
+i
